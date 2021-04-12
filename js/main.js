9bf2f945994c2284374904f4d71e71dcd5accf70
mattius9 <matthew.krasucki@gmail.com>
2021-04-12 12:17:17 -0400

    Added event listeners

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
+//Variable Declarations
+let board = [];
+let player1Checkers = []; 
+let player2Checkers = [];
 
+// Cached DOM Elements
+player1Checkers = document.getElementsByClassName('player1-checker');
+console.log(player1Checkers);
+
+player2Checkers = document.getElementsByClassName('player2-checker');
+console.log(player2Checkers);
+
+board = document.getElementsByClassName('board');
+console.log(board);
+
+// let playableSquares = document.querySelectorAll('tr:nth-of-type(2n) > td:nth-of-type(2n+1)','tr:nth-of-type(2n+1) > td:nth-of-type(2n)');
+// console.log(playableSquares);
+
+// Event Listeners
+
+// for (i of player1Checkers){
+//     i.addEventListener('click', function(){
+//         console.log(this);
+//         console.log('event listener added');
+//     })
+// }
+
+document.getElementsByClassName('player1-checker')[0].addEventListener('click', function(){
+    console.log(this);
+    console.log('event listener added');
+});
 
 
 // Initialize the game board and place checkers in starting positions
@@ -6,6 +36,9 @@ function init(){
     
 }
 
+function render(){
+
+}
 
 
 // function showPosition(elem){
