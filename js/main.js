3315d339e8ae37e945918717f54d1ce3aa3d07b4
mattius9 <matthew.krasucki@gmail.com>
2021-04-11 20:21:45 -0400

    Designed Checker board

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// function showPosition(elem){
+//     console.log(`function is running `);
+//     elem.textContent = `${elem.offsetTop},${elem.offsetWidth}`;
+//     console.log(`${elem.offsetTop},${elem.offsetWidth}`)
+//     console.log("function ran");
+// }
+
+// const board = document.querySelectorAll('td');
+
+// let mainEl = document.querySelector('main');
+// console.log(mainEl.textContent);
+
+// board.forEach(showPosition);
+
+// console.log(board);
+
+// let checkerEl = document.querySelector('div');
+
+// let cellEl = document.querySelector('#first-cell');
+
+// cellEl.appendChild(checkerEl);
+
