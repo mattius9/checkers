cba49e35b143cf4e9c3024320d0f8bf1c863dd19
mattius9 <matthew.krasucki@gmail.com>
2021-04-13 13:04:36 -0400

    Add position references in HTML table, and boardState array

--- js/main.js
+++ js/main.js
@@ -8,7 +8,15 @@ let boardState = [[{},{},{},{},{},{},{},{}], // 8 x 8 array of board tiles (y,x)
 [{},{},{},{},{},{},{},{}],
 [{},{},{},{},{},{},{},{}]]; 
 
-//Set unplayable tiles
+//boardState objects should appear like this:
+
+// boardObject = {
+//     isPlayable : (true/false)
+//     checker : 0, 1, or 2
+//     position : 01 - 76
+// }
+
+//Set unplayable tiles (for drag and drop movement - not yet implemented)
 for(let i = 0; i < boardState.length; i++){
     for(let j = 0; j < boardState[i].length; j++){
         if (i%2 == 0){
@@ -24,6 +32,28 @@ for(let i = 0; i < boardState.length; i++){
     }
 }
 
+//Setting position values
+for(let i = 0; i < boardState.length; i++){
+    for(let j = 0; j < boardState[i].length; j++){
+        if (i%2 == 0){
+            if (j%2 == 0){
+                boardState[i][j].isPlayable = false;
+            }
+            else{
+                boardState[i][j].position = `${i}${j}`;
+            }
+        }
+        else if (i%2 != 0){
+            if (j%2 !=0){
+                boardState[i][j].isPlayable = false;
+            }
+            else{
+                boardState[i][j].position = `${i}${j}`;
+            }
+        }
+    }
+}
+console.dir(boardState);
 // Set player 2 checkers 
 for(let i = 0; i < 3; i++){
     for(let j = 0; j < boardState[i].length; j++){
@@ -56,6 +86,7 @@ for(let i = 5; i < boardState.length; i++){
     }
 }
 
+player = 1;
 
 //Board printer:
 // for(let i = 0; i < boardState.length; i++){
@@ -73,18 +104,28 @@ let player2CheckersEl = document.getElementsByClassName('player2-checker');
 
 let boardEl = document.getElementsByTagName('td');
 
+let player1 = [{}];
 
+let player2 = [{}];
 // Event Listeners
 
-for(i of player1CheckersEl){
-    i.addEventListener('click', function(){
-        console.log('event listener activated');// Add functionality here
-    })
-}
+for(i of boardEl){
+    i.addEventListener('click', function(e){
+        let pos=e.target.parentElement.id;
+        let boardTile = boardState.find(function(o){
+            return o.position == pos;
+        });
+        if(player == 1 && boardTile.checker == 1){
+            // If it's player 1 turn and a player 1 piece is selected
+            
+        }
+        else if(player == 2 && boardTile.checker == 2){
+            // If it's player 2 turn and a player 2 piece is selected
 
-for(i of player2CheckersEl){
-    i.addEventListener('click', function(){
-        console.log('event listener activated');// Add functionality here
+        }
+        else{
+            alert('not your turn');
+        }
     })
 }
 
@@ -105,6 +146,20 @@ function render(){
 }
 
 function playerTurn(player){
+
+    //selectPiece
+
+    //selectMove
+
+    //player = nextPlayer
+    
+}
+
+function selectPiece(){
+ //showMoves
+}
+
+showMoves(e){
 
 }
 
