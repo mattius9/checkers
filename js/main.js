3a1b2ad2580d289c2abcd89418c70e0a67260e5c
mattius9 <matthew.krasucki@gmail.com>
2021-04-14 11:21:12 -0400

    checkers now move, no conditions

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ let boardState = [[{},{},{},{},{},{},{},{}], // 8 x 8 array of board tiles (y,x)
 [{},{},{},{},{},{},{},{}],
 [{},{},{},{},{},{},{},{}]]; 
 
+let prevPos = [];
+
+let movePos;
+
 //boardState objects should appear like this:
 
 // boardObject = {
@@ -70,6 +74,22 @@ for(let i = 0; i < 3; i++){
     }
 }
 
+// Set empty playable checkers
+for(let i = 3; i <5; i++){
+    for(let j = 0; j <boardState[i].length; j++){
+        if (i%2 !== 0){
+            if (j%2 == 0){
+                boardState[i][j].checker = '0';
+            }
+        }
+        else if (i%2 == 0){
+            if (j%2 !== 0){
+                boardState[i][j].checker = '0';
+            }
+        }
+    }
+}
+
 // Set player 1 checkers
 for(let i = 5; i < boardState.length; i++){
     for(let j = 0; j < boardState[i].length; j++){
@@ -85,48 +105,57 @@ for(let i = 5; i < boardState.length; i++){
         }
     }
 }
-
+// Set player statically temporarily *** DELETE THIS ***
 player = 1;
-
-//Board printer:
-// for(let i = 0; i < boardState.length; i++){
-//     for(let j = 0; j < boardState[i].length; j++){
-//         if(boardState[i][j].checker){
-//             console.log(`Board at ${i} and ${j} contains a checker for player ${boardState[i][j].checker}`)
-//         }
-//     }
-// }
 
 // Cached DOM Elements
 let player1CheckersEl = document.getElementsByClassName('player1-checker');
 
 let player2CheckersEl = document.getElementsByClassName('player2-checker');
 
-let boardEl = document.getElementsByTagName('td');
+// let boardEl = document.getElementsByTagName('td');
 
-let player1 = [{}];
-
-let player2 = [{}];
 // Event Listeners
 
-for(i of boardEl){
-    i.addEventListener('click', function(e){
-        let pos=e.target.parentElement.id;
-        let boardTile = boardState.find(function(o){
-            return o.position == pos;
-        });
-        if(player == 1 && boardTile.checker == 1){
-            // If it's player 1 turn and a player 1 piece is selected
-            
-        }
-        else if(player == 2 && boardTile.checker == 2){
-            // If it's player 2 turn and a player 2 piece is selected
+for(i of player1CheckersEl){
+    i.addEventListener('click', selectCheckerHandler);
+}    
 
+for(i of player2CheckersEl){
+    i.addEventListener('click', selectCheckerHandler);
+}    
+    
+
+function showMoves(checkerTile, player){
+
+    let pos = parseInt(checkerTile.id);
+    if (player == 1){
+        let moveLeft = pos-11;
+        let moveRight = pos-9;
+        let moveLeftEl = document.getElementById(`${moveLeft}`);
+        let moveRightEl = document.getElementById(`${moveRight}`);
+        if(moveLeftEl){
+            moveLeftEl.classList.add('available-moves');
         }
-        else{
-            alert('not your turn');
+        if(moveRightEl){
+            moveRightEl.classList.add('available-moves');
         }
-    })
+        return [moveLeftEl,moveRightEl];
+    }
+    else if(player ==2){
+        let moveLeft = pos+11;
+        let moveRight = pos+9;
+        let moveLeftEl = document.getElementById(`${moveLeft}`);
+        let moveRightEl = document.getElementById(`${moveRight}`);
+        if(moveLeftEl){
+            moveLeftEl.classList.add('available-moves');
+        }
+        if(moveRightEl){
+            moveRightEl.classList.add('available-moves');
+        }
+        return [moveLeftEl,moveRightEl]; 
+    }
+    
 }
 
 // Initialize the game board and place checkers in starting positions
@@ -159,7 +188,68 @@ function selectPiece(){
  //showMoves
 }
 
-showMoves(e){
+function selectCheckerHandler(e){
+        let pieceTile = e.target.parentElement;
+        let pos=parseInt(pieceTile.id);
+        
+        let i = Math.trunc(pos/10);
+        let j = pos%10;
+        prevPos.push({i:i,j:j});
+        // console.log(`Previous position is ${prevPos[0]}, ${prevPos[1]}`);
+        let boardTile = boardState[i][j];
+        if(player == 1 && boardTile.checker == 1){
+        // If it's player 1 turn and a player 1 piece is selected
+        // available moves are i - 1, j +/- 1
+        let movesAvailable = showMoves(pieceTile,player); //returns the tiles available to move to
+        for(m of movesAvailable){
+            if (m)m.addEventListener('click',selectMoveHandler);
+        }
+        
 
+
+
+    }
+    else if(player == 2 && boardTile.checker == 2){
+        // If it's player 2 turn and a player 2 piece is selected
+        // available moves are i + 1, j+/- 1
+        let movesAvailable = showMoves(pieceTile,player); //returns the tiles available to move to
+        for(m of movesAvailable){
+            if(m)m.addEventListener('click',selectMoveHandler);
+        }
+
+    }
+    else{
+        //alert('not your turn');
+    }
+
+        
 }
+
+function selectMoveHandler(e){
+    
+   
+    
+    //console.log(`Previous position is: ${prevPos[prevPos.length-1].i},${prevPos[prevPos.length-1].j}`);
+    let moveTile = e.target;
+    let prevTileEl = document.getElementById(`${prevPos[prevPos.length-1].i}${prevPos[prevPos.length-1].j}`);
+    let moveTileEl = document.getElementById(moveTile.id);
+    // Take the checker piece element from the previous tile to the chosen tile
+    moveTileEl.innerHTML = prevTileEl.innerHTML;
+    prevTileEl.innerHTML = "";
+
+    let pos=parseInt(moveTile.id);
+    
+    // console.log(`Tile ID ${pos}`);
+    
+    let i = Math.trunc(pos/10);
+    let j = pos%10;
+    movePos = {i:i,j:j};
+    // console.log(`movePos i is ${movePos.i}, movePos j is ${movePos.j}`)
+    // console.log(`${boardState[i][j].checker}`);
+    boardState[i][j].checker = player;
+    
+    
+    return;
+}
+
 
